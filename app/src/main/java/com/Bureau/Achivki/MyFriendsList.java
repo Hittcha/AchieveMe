02b26cc13d3ad59b655c53691aa0c3dbf92d33c2
Sergey <sergeygerasimov.e@yandex.ru>
2023-05-31 09:52:45 +0200

    0.26.4

--- app/src/main/java/com/Bureau/Achivki/MyFriendsList.java
+++ app/src/main/java/com/Bureau/Achivki/MyFriendsList.java
@@ -1,10 +1,5 @@
 package com.Bureau.Achivki;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.content.ContextCompat;
-
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -12,44 +7,34 @@ import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Shader;
-import android.hardware.display.DisplayManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
 public class MyFriendsList extends AppCompatActivity {
-
     private FirebaseFirestore db;
-
     private String userName;
 
     @Override
@@ -79,97 +64,60 @@ public class MyFriendsList extends AppCompatActivity {
 
         DocumentReference mAuthDocRef = db.collection("Users").document(currentUser.getUid());
 
-        mAuthDocRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-            @Override
-            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                if (documentSnapshot.exists()) {
-                    userName = documentSnapshot.getString("name");
+        mAuthDocRef.get().addOnSuccessListener(documentSnapshot -> {
+            if (documentSnapshot.exists()) {
+                userName = documentSnapshot.getString("name");
 
-                    //userScore = documentSnapshot.getLong("score");
+                Map<String, Object> userData = documentSnapshot.getData();
+                Map<String, Object> friends = (Map<String, Object>) userData.get("friends");
+                for (Map.Entry<String, Object> entry : friends.entrySet()) {
+                    Map<String, Object> friend = (Map<String, Object>) entry.getValue();
+                    String key = entry.getKey();
+                    System.out.println("key: " + key);
 
-                    //System.out.println("score " + userScore);
+                    String friendName = (String) friend.get("name");
+                    String friendAvatar = (String) friend.get("avatar");
 
-                    //profileImageUrl = documentSnapshot.getString("profileImageUrl");
+                    System.out.println("friendName: " + friendName);
+                    System.out.println("friendAvatar: " + friendAvatar);
 
-                    //userNameText.setText(userName);
+                    createFriendBlock(friendName, friendAvatar, mAuthDocRef, key, userID);
 
-                    //userScoreText.setText("" + userScore);
-                    // использовать имя пользователя
-                    //setImage(profileImageUrl);
-
-
-                    Map<String, Object> userData = documentSnapshot.getData();
-                    Map<String, Object> friends = (Map<String, Object>) userData.get("friends");
-                    for (Map.Entry<String, Object> entry : friends.entrySet()) {
-                        Map<String, Object> friend = (Map<String, Object>) entry.getValue();
-                        String key = entry.getKey();
-                        System.out.println("key: " + key);
-
-                        String friendName = (String) friend.get("name");
-                        String friendAvatar = (String) friend.get("avatar");
-
-                        System.out.println("friendName: " + friendName);
-                        System.out.println("friendAvatar: " + friendAvatar);
-
-                        createFriendBlock(friendName, friendAvatar, mAuthDocRef, key, userID);
-
-                    }
-
-                } else {
-                    // документ не найден
                 }
+            } else {
+                // документ не найден
             }
         });
 
         ImageButton leaderListButton = findViewById(R.id.imageButtonLeaderList);
-
         ImageButton menuButton = findViewById(R.id.imageButtonMenu);
-
         ImageButton favoritesButton = findViewById(R.id.imageButtonFavorites);
-
         ImageButton achieveListButton = findViewById(R.id.imageButtonAchieveList);
 
-        leaderListButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MyFriendsList.this, LeaderBoardActivity.class);
-                startActivity(intent);
-            }
+        leaderListButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MyFriendsList.this, LeaderBoardActivity.class);
+            startActivity(intent);
         });
 
-        menuButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MyFriendsList.this, MainActivity.class);
-                startActivity(intent);
-            }
+        menuButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MyFriendsList.this, MainActivity.class);
+            startActivity(intent);
         });
 
-        favoritesButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MyFriendsList.this, ListOfFavoritesActivity.class);
-                startActivity(intent);
-            }
+        favoritesButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MyFriendsList.this, ListOfFavoritesActivity.class);
+            startActivity(intent);
         });
 
-        achieveListButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MyFriendsList.this, AchieveListActivity.class);
-                startActivity(intent);
-            }
+        achieveListButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MyFriendsList.this, AchieveListActivity.class);
+            startActivity(intent);
         });
 
         ImageButton usersListButton = findViewById(R.id.imageButtonUsersList);
-        usersListButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MyFriendsList.this, UsersListActivity.class);
-                //User user = new User("Имя пользователя", 1);
-                //intent.putExtra("user", user);
-                startActivity(intent);
-            }
+        usersListButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MyFriendsList.this, UsersListActivity.class);
+            startActivity(intent);
         });
     }
 
@@ -181,96 +129,64 @@ public class MyFriendsList extends AppCompatActivity {
                 .inflate(R.layout.block_friends, parentLayout, false);
 
         TextView usernameTextView = blockFriends.findViewById(R.id.userName);
-        //TextView balanceTextView = blockFriends.findViewById(R.id.date);
-        //TextView likesTextView = blockFriends.findViewById(R.id.likesCount);
 
         usernameTextView.setText(name);
 
         parentLayout.addView(blockFriends);
 
-
         Button delButton = blockFriends.findViewById(R.id.delete_button);
 
+        delButton.setOnClickListener(v -> {
+            System.out.println("del " + name);
+            mAuthDocRef.get().addOnSuccessListener(documentSnapshot -> {
+                Map<String, Object> userFriends = documentSnapshot.getData();
 
-        delButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                System.out.println("del " + name);
-                mAuthDocRef.get().addOnSuccessListener(documentSnapshot -> {
-                    Map<String, Object> userFriends = documentSnapshot.getData();
+                Map<String, Object> friendMap = (Map<String, Object>) userFriends.get("friends");
 
-                    Map<String, Object> friendMap = (Map<String, Object>) userFriends.get("friends");
+                friendMap.remove(key);
 
-                    friendMap.remove(key);
+                userFriends.put("friends", friendMap);
 
-                    userFriends.put("friends", friendMap);
+                userFriends.put("friendscount", Long.valueOf(friendMap.size()));
 
-                    userFriends.put("friendscount", Long.valueOf(friendMap.size()));
+                mAuthDocRef.set(userFriends);
 
-                    mAuthDocRef.set(userFriends);
+                parentLayout.removeView(blockFriends);
 
-                    parentLayout.removeView(blockFriends);
+                delFollower(key, userID);
 
-                    //delScore(userName);
+                Toast.makeText(MyFriendsList.this, "Пользователь удален из подписок", Toast.LENGTH_SHORT).show();
+            });
 
-                    delFollower(key, userID);
-
-                    Toast.makeText(MyFriendsList.this, "Пользователь удален из подписок", Toast.LENGTH_SHORT).show();
-                });
-
-            }
         });
 
         ImageButton imageUserButton = blockFriends.findViewById(R.id.imageUserButton);
 
-        imageUserButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MyFriendsList.this, OtherUserActivity.class);
-                intent.putExtra("User_token", key);
-                startActivity(intent);
-            }
+        imageUserButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MyFriendsList.this, OtherUserActivity.class);
+            intent.putExtra("User_token", key);
+            startActivity(intent);
         });
 
         StorageReference storageRef = FirebaseStorage.getInstance().getReference();
         StorageReference userImageRef = storageRef.child(avatarUrl);
         try {
             final File localFile = File.createTempFile("images", "jpg");
-            userImageRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
-                    Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+            userImageRef.getFile(localFile).addOnSuccessListener(taskSnapshot -> {
+                Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
 
-                    // определяем размеры экрана
-                    /*DisplayMetrics displayMetrics = new DisplayMetrics();
-                    Display display = ContextCompat.getSystemService(MyFriendsList.this, DisplayManager.class).getDisplay(Display.DEFAULT_DISPLAY);
+                Bitmap circleBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+                BitmapShader shader = new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+                Paint paint = new Paint();
+                paint.setShader(shader);
 
-
-                    int targetWidth = getResources().getDisplayMetrics().widthPixels / 3;
-
-
-                    int targetHeight = targetWidth;
-                    Bitmap resizedBitmap = Bitmap.createScaledBitmap(bitmap, targetWidth, targetHeight, true);
-*/
-
-                   // Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-                    Bitmap circleBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
-                    BitmapShader shader = new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
-                    Paint paint = new Paint();
-                    paint.setShader(shader);
-
-                    Canvas canvas = new Canvas(circleBitmap);
-                    if (bitmap.getHeight() > bitmap.getWidth()){
-                        canvas.drawCircle(bitmap.getWidth() / 2f, bitmap.getHeight() / 2f, bitmap.getWidth() / 2f, paint);
-                    }else{
-                        canvas.drawCircle(bitmap.getWidth() / 2f, bitmap.getHeight() / 2f, bitmap.getHeight() / 2f, paint);
-                    }
-
-
-                    imageUserButton.setImageBitmap(circleBitmap);
-
-                    //imageView.setAdjustViewBounds(true);
+                Canvas canvas = new Canvas(circleBitmap);
+                if (bitmap.getHeight() > bitmap.getWidth()){
+                    canvas.drawCircle(bitmap.getWidth() / 2f, bitmap.getHeight() / 2f, bitmap.getWidth() / 2f, paint);
+                }else{
+                    canvas.drawCircle(bitmap.getWidth() / 2f, bitmap.getHeight() / 2f, bitmap.getHeight() / 2f, paint);
                 }
+                imageUserButton.setImageBitmap(circleBitmap);
             });
         } catch (IOException e) {
             e.printStackTrace();
--- app/src/main/java/com/Bureau/Achivki/OtherUserFriends.java
+++ app/src/main/java/com/Bureau/Achivki/OtherUserFriends.java
@@ -41,11 +41,9 @@ public class OtherUserFriends extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other_user_friends);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(ContextCompat.getColor(this, R.color.StatusBarColor));
-        }
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.StatusBarColor));
 
         androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
 
